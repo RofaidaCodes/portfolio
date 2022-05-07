52cf31ef3278ce6789562e7beb6a2ddc58475140
rofaida <aygratte@gmail.com>
2022-05-07 22:36:20 +0100

    v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useCallback} from "react";
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Navbar from './components/navbar.js';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 function App() {
+ 
   return (
+    
+    <ParallaxProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Home></Home>
+      <About></About>
+      <Projects></Projects>
+      <Contact></Contact>
     </div>
+    </ParallaxProvider>
   );
 }
 
